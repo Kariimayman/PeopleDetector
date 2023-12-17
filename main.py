7221a616a682f4f50bd4dd3da6bcb62963af5da9
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2023-12-17 11:18:18 +0200

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import os
+import shutil
+from ultralytics import YOLO
+import cv2
+import torch
+print(torch.cuda.is_available())
+conf = 0.8
+framesskip = 5
+testmode = 0
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def ProduceImg(videopath):
+    video = cv2.VideoCapture(videopath)
+    ret, frame = video.read()
+    model1 = YOLO("ModelV5.pt")
+    count = framesskip
+    ids = []
+    while ret:
+        result1 = model1.track(frame, persist=True, conf=0.8)
+        for box in result1[0].boxes:
+            x = box.xyxy.tolist()
+            if testmode == 1:
+                frame = cv2.putText(frame, str(box.conf), (int(x[0][0]), int(x[0][1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                                    (0, 0, 255), 1, cv2.LINE_AA)
+                frame = cv2.rectangle(frame, (int(x[0][0]), int(x[0][1])), (int(x[0][2]), int(x[0][3])), (0, 255, 0), 1)
+            id = int(box.id)
+            if id > len(ids):
+                ids.append([box.conf, result1[0].orig_img[int(x[0][1]):int(x[0][3]), int(x[0][0]):int(x[0][2])]])
+            else:
+                if box.conf > ids[id - 1][0]:
+                    ids[id - 1][0] = box.conf
+                    ids[id - 1][1] = result1[0].orig_img[int(x[0][1]):int(x[0][3]), int(x[0][0]):int(x[0][2])]
+        if testmode == 1:
+            try:
+                frame = cv2.resize(frame, (700, 700))
+                cv2.imshow("frame", frame)
+            except():
+                continue
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        count += framesskip
+        video.set(cv2.CAP_PROP_POS_FRAMES, count)
+        ret, frame = video.read()
+        cv2.waitKey(2)
 
+    for i in range(len(ids)):
+        print(str(ids[i][0]))
+        cv2.imwrite(str(i+1)+"-"+videopath[:-4]+'.jpg', ids[i][1])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ProduceImg("1_2750_20230911_111935246.avi")
